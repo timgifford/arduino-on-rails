46a9c3e9fc32e71e9522a822be420371d5327d2d
Tim Gifford <tgifford@gmail.com>
2013-05-25 11:23:53 -0500

    checking in original.ino before working on sending

--- src/original/original.ino
+++ src/original/original.ino
@@ -1,7 +1,9 @@
-// #include <LocoNet.h>
+#include <LocoNet.h>
 #include <SerialLCD.h>
 #include <SoftwareSerial.h>
 #include <Keypad.h>
+
+lnMsg        *LnPacket;
 
 SerialLCD slcd(11,12);//this is a must, assign soft serial pins
 
@@ -29,11 +31,21 @@ char keys[ROWS][COLS] = {
   | | | |  |  |  |
   | | | |  |  |  |
   | | | |  |  |  |
-  8 7 6 5  4  3  2 (pins)
+ 10 9 6 5  4  3  2 (pins)
+*/
+
+/*
+LED Pin 11
+*/
+
+/* LocoNet board Wiring (http://www.scuba.net/wiki/index.php/LocoShield)
+Ground goes to Ground, Vcc goes to a source of 5v power
+TX goes to D7 on your Arduino, and
+RX goes to pin D8
 */
 
 // Connect keypad Arduino pins.
-byte rowPins[ROWS] = { 8, 7, 6, 5 };
+byte rowPins[ROWS] = { 10, 9, 6, 5 };
 byte colPins[COLS] = { 4, 3, 2 };
 Keypad keyPad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 
@@ -41,10 +53,19 @@ Keypad keyPad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 void setup(){
   slcd.begin();
   slcd.backlight();
-  // LocoNet.init()
+  
+    // First initialize the LocoNet interface
+  LocoNet.init();
+
+  // Configure the serial port for 57600 baud
+  Serial.begin(57600);
+  Serial.println("LocoNet Monitor");
 }
 
 void loop(){
+  
+  
+  receiveLocoNetMessage();
   
   char customKey = keyPad.getKey();
   
@@ -52,6 +73,22 @@ void loop(){
     if(isNumber(customKey)){
       slcd.backlight();
       slcd.print(customKey);
+      
+//      ( uint16_t Address, uint8_t Output, uint8_t Direction ) ;
+      uint16_t Address = 1;
+      uint8_t Output = 0;
+      uint8_t Direction = 0;
+      uint8_t output = 0;
+
+      
+      Serial.print("requestSwitch: ");
+      Serial.print(Address);
+      Serial.print(":");
+      Serial.print(Direction);
+      Serial.print(" - ");
+      Serial.println(Output);
+      LocoNet.requestSwitch(1,0,0);
+      
     }
     else if(isClear(customKey)){
       slcd.clear();
@@ -69,6 +106,67 @@ boolean isNumber(char key){
 
 boolean isClear(char key){
   return (key == '*');
+}
+
+void receiveLocoNetMessage(){
+// Check for any received LocoNet packets
+  LnPacket = LocoNet.receive() ;
+  if( LnPacket )
+  {
+      // First print out the packet in HEX
+    Serial.print("RX: ");
+    uint8_t msgLen = getLnMsgSize(LnPacket); 
+    for (uint8_t x = 0; x < msgLen; x++)
+    {
+      uint8_t val = LnPacket->data[x];
+        // Print a leading 0 if less than 16 to make 2 HEX digits
+      if(val < 16)
+        Serial.print('0');
+        
+      Serial.print(val, HEX);
+      Serial.print(' ');
+    }
+    
+      // If this packet was not a Switch or Sensor Message then print a new line 
+    if(!LocoNet.processSwitchSensorMessage(LnPacket))
+      Serial.println();
+  }
+}
+
+  // This call-back function is called from LocoNet.processSwitchSensorMessage
+  // for all Switch Request messages
+void notifySwitchRequest( uint16_t Address, uint8_t Output, uint8_t Direction )
+{
+  Serial.print("Switch Request: ");
+  Serial.print(Address, DEC);
+  Serial.print(':');
+  Serial.print(Direction ? "Closed" : "Thrown");
+  Serial.print(" - ");
+  Serial.println(Output ? "On" : "Off");
+}
+
+  // This call-back function is called from LocoNet.processSwitchSensorMessage
+  // for all Switch Report messages
+void notifySwitchReport( uint16_t Address, uint8_t Output, uint8_t Direction )
+{
+  Serial.print("Switch Report: ");
+  Serial.print(Address, DEC);
+  Serial.print(':');
+  Serial.print(Direction ? "Closed" : "Thrown");
+  Serial.print(" - ");
+  Serial.println(Output ? "On" : "Off");
+}
+
+  // This call-back function is called from LocoNet.processSwitchSensorMessage
+  // for all Switch State messages
+void notifySwitchState( uint16_t Address, uint8_t Output, uint8_t Direction )
+{
+  Serial.print("Switch State: ");
+  Serial.print(Address, DEC);
+  Serial.print(':');
+  Serial.print(Direction ? "Closed" : "Thrown");
+  Serial.print(" - ");
+  Serial.println(Output ? "On" : "Off");
 }
 
 
